3d68d6732c41cb193c39860497a1b0517ba9e6bd
Sruthi <msruthi251999@gmail.com>
2026-07-17 20:18:58 -0400

    Added Auth Interceptor file

---
+++ FrontEnd/finance-tracker/src/app/auth/auth.interceptor.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, Observable, throwError } from "rxjs";
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private router: Router) { }
+
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        const cloned = req.clone({
+            withCredentials: true
+        });
+
+        return next.handle(cloned).pipe(
+            catchError((error: HttpErrorResponse) => {
+
+                if (error.status == 401) {
+                    this.router.navigate(['/login']);
+                }
+
+                return throwError(() => error);
+            })
+        )
+    }
+
+
+
+}
